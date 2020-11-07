dcb382675a0d1ae7aeb7ef1cef41f6439e8067e8
bertdk <bertdk10@gmail.com>
2020-11-08 00:58:58 +0100

    feat: message on screen

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,30 @@
 const socket = io();
 
 // Elements
+const $messageForm = document.getElementById('messageForm');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocation = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+// Templates
+const $messageTemplate = document.querySelector('#message-template').innerHTML;
 
 socket.on('message', (message) => {
-  console.log('message received', message);
+  const html = Mustache.render($messageTemplate, {
+    message,
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
-document.querySelector('#messageForm').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  $messageFormButton.setAttribute('disabled', 'disabled');
   const message = e.target.elements.message.value;
   socket.emit('messageSend', message, (error) => {
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
     if (error) {
       return console.log('ack', error);
     }
@@ -17,13 +32,15 @@ document.querySelector('#messageForm').addEventListener('submit', (e) => {
   });
 });
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocation.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported for you browser.');
   }
+  $sendLocation.setAttribute('disabled', 'disabled');
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit('sendLocation', { longitude: position.coords.longitude, latitude: position.coords.latitude }, () => {
       console.log('Location send');
+      $sendLocation.removeAttribute('disabled');
     });
   });
 });
